4329fa707154db278caf798b4e804c4e42e9e73b
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2023-07-25 10:16:03 +0200

    Day 9 part 2 incorrectly

--- src/com/adventOfCode/day9/RopeBridge.java
+++ src/com/adventOfCode/day9/RopeBridge.java
@@ -7,30 +7,50 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class RopeBridge implements AoC {
+    private static final int NODES_MAX = 10;
+    private static final double SQRT_OF_2 = Math.sqrt(2);
     private final List<String> commands;
-    private final double sqrtOf2 = Math.sqrt(2);
-    private final List<Position> tailPositionsList = new ArrayList<>();
+    private final List<Position> tailPositions = new ArrayList<>();
+    private final List<Position> ropeTailPositions = new ArrayList<>();
     private final Node head;
     private final Node tail;
-    private static final int boundries= 100;
+    private final List<Node> nodes = new ArrayList<>();
 
     public RopeBridge() throws FileNotFoundException {
         commands = new Scan(this.getClass()).createFileList();
         head = new Node(new Position(0, 0));
         tail = new Node(new Position(0, 0));
-        tailPositionsList.add(new Position(0, 0));
+        tailPositions.add(new Position(0, 0));
+        ropeTailPositions.add(new Position(0, 0));
+        nodes.add(head);
+        for (int i = 1; i < NODES_MAX; i++) {
+            nodes.add(new Node(new Position(0, 0)));
+        }
     }
 
     private void operate() {
         for (String command : commands) {
-            Vector vector = decryptCommand(command);
-            for (int i = 0; i < vector.getValue(); i++) {
-                head.moveBy1(vector.getDirection());
-                if (calcDistance() > sqrtOf2) {
-                    tail.setPosition(calculateTailPosition(vector));
-                    addTailPositionToList();
+            Vector mainVector = decryptCommand(command);
+            for (int i = 0; i < mainVector.getValue(); i++) {
+                head.moveBy1(mainVector.getDirection());
+                if (calcDistance(head, tail) > SQRT_OF_2) {
+                    tail.setPosition(calculateTailPosition(mainVector, head, tail));
+                    addTailPositionToList(tail, tailPositions);
+                }
+                Vector vector = mainVector;
+                for (int j = 1; j < NODES_MAX; j++) {
+                    if (calcDistance(nodes.get(j - 1), nodes.get(j)) > SQRT_OF_2) {
+                        Position positionBeforeMove = nodes.get(j).getPosition();
+                        nodes.get(j).setPosition(calculateTailPosition(vector, nodes.get(j - 1), nodes.get(j)));
+                        Position positionAfterMove = nodes.get(j).getPosition();
+                        vector = Vector.calculateVector(positionAfterMove, positionBeforeMove);
+                        if (j == NODES_MAX - 1) {
+                            addTailPositionToList(nodes.get(j), ropeTailPositions);
+                        }
+                    }
                 }
                // view();
             }
@@ -38,45 +58,47 @@ public class RopeBridge implements AoC {
     }
 
     private void view() {
-        char[][] field = new char[boundries][boundries];
-        for (int i = 0; i < boundries; i++) {
-            for (int j = 0; j < boundries; j++) {
+        int border = 50;
+        char[][] field = new char[border][border];
+        for (int i = 0; i < border; i++) {
+            for (int j = 0; j < border; j++) {
                 field[i][j] = '.';
             }
         }
-        field[head.getPosition().getPositionX()][head.getPosition().getPositionY()] = 'h';
-        field[tail.getPosition().getPositionX()][tail.getPosition().getPositionY()] = 't';
-        for (int i = 0; i < boundries; i++) {
+        for (int i = 0; i < NODES_MAX; i++) {
+            field[nodes.get(i).getPosition().getPositionX() + border / 2][nodes.get(i).getPosition().getPositionY() + border / 2] = i == 0 ? 'h' : (char) (i + 48);
+        }
+        for (int i = 0; i < border; i++) {
             System.out.println(Arrays.toString(field[i]));
         }
         System.out.println("next");
     }
 
-    private void addTailPositionToList() {
+    private void addTailPositionToList(Node tail, List<Position> tailPositions) {
         Position tailPosition = tail.getPosition();
-        for (Position position : tailPositionsList) {
+        for (Position position : tailPositions) {
             if (position.getPositionX() == tailPosition.getPositionX() && position.getPositionY() == tailPosition.getPositionY()) {
                 return;
             }
         }
-        tailPositionsList.add(tailPosition);
+        tailPositions.add(tailPosition);
     }
 
-    private Position calculateTailPosition(Position vector) {
-        if (vector.getPositionX() < 0) {
-            return new Position(head.getPosition().getPositionX() + 1, head.getPosition().getPositionY());
-        } else if (vector.getPositionX() > 0) {
-            return new Position(head.getPosition().getPositionX() - 1, head.getPosition().getPositionY());
-        } else if (vector.getPositionY() < 0) {
-            return new Position(head.getPosition().getPositionX(), head.getPosition().getPositionY() + 1);
-        } else if (vector.getPositionY() > 0) {
-            return new Position(head.getPosition().getPositionX(), head.getPosition().getPositionY() - 1);
-        } else {
-            return tail.getPosition();
-        }
+    private Position calculateTailPosition(Vector vector, Node head, Node tail) {
+        return switch (vector.getDirection()) {
+            case R -> new Position(head.getPosition().getPositionX() - 1, head.getPosition().getPositionY());
+            case L -> new Position(head.getPosition().getPositionX() + 1, head.getPosition().getPositionY());
+            case U -> new Position(head.getPosition().getPositionX(), head.getPosition().getPositionY() - 1);
+            case D -> new Position(head.getPosition().getPositionX(), head.getPosition().getPositionY() + 1);
+            case UR -> new Position(head.getPosition().getPositionX() - 1, head.getPosition().getPositionY() - 1);
+            case UL -> new Position(head.getPosition().getPositionX() + 1, head.getPosition().getPositionY() - 1);
+            case DR -> new Position(head.getPosition().getPositionX() - 1, head.getPosition().getPositionY() + 1);
+            case DL -> new Position(head.getPosition().getPositionX() + 1, head.getPosition().getPositionY() + 1);
+            case NONE -> tail.getPosition();
+        };
     }
 
-    private double calcDistance() {
+    private double calcDistance(Node head, Node tail) {
         return Math.sqrt(Math.pow(head.getPosition().getPositionX() - tail.getPosition().getPositionX(), 2)
                 + Math.pow(head.getPosition().getPositionY() - tail.getPosition().getPositionY(), 2));
     }
@@ -89,17 +111,18 @@ public class RopeBridge implements AoC {
             case L -> new Vector(-steps, 0, direction);
             case U -> new Vector(0, steps, direction);
             case D -> new Vector(0, -steps, direction);
+            case UR, DL, UL, DR, NONE -> null;
         };
     }
 
     @Override
     public Object getResultPart1() {
-        return tailPositionsList.size();
+        return tailPositions.size();
     }
 
     @Override
     public Object getResultPart2() {
-        return null;
+        return ropeTailPositions.size();
     }
 
     @Override
--- src/com/adventOfCode/day9/Vector.java
+++ src/com/adventOfCode/day9/Vector.java
@@ -2,10 +2,10 @@ package com.adventOfCode.day9;
 
 public class Vector extends Position {
     public enum Direction {
-        U, D, R, L;
+        U, D, R, L, NONE, UR, UL, DR, DL
     }
 
-    private final Direction direction;
+    private Direction direction;
     private final int value;
 
     public Vector(int positionX, int positionY, Direction direction) {
@@ -14,22 +14,85 @@ public class Vector extends Position {
         this.direction = direction;
     }
 
+    public Vector(Position position) {
+        value = 1;
+        switch (position.getPositionX() + position.getPositionY()) {
+            case 1 -> {
+                if (position.getPositionX() != 0) {
+                    direction = Direction.R;
+                } else {
+                    direction = Direction.U;
+                }
+            }
+            case -1 -> {
+                if (position.getPositionX() != 0) {
+                    direction = Direction.L;
+                } else {
+                    direction = Direction.D;
+                }
+            }
+            case 2 -> direction = Direction.UR;
+            case -2 -> direction = Direction.DL;
+            case 0 -> {
+                if (position.getPositionX() < 0) {
+                    direction = Direction.UL;
+                } else if (position.getPositionX() > 0) {
+                    direction = Direction.DR;
+                } else {
+                    direction = Direction.NONE;
+                }
+            }
+        }
+    }
+
     public Vector(Direction direction) {
         value = 1;
         this.direction = direction;
-        if (direction.equals(Direction.R)) {
-            setPositionX(1);
-            setPositionY(0);
-        } else if (direction.equals(Direction.L)) {
-            setPositionX(-1);
-            setPositionY(0);
-        } else if (direction.equals(Direction.U)) {
-            setPositionX(0);
-            setPositionY(1);
-        } else if (direction.equals(Direction.D)) {
-            setPositionX(0);
-            setPositionY(-1);
+        switch (direction) {
+            case R -> {
+                setPositionX(1);
+                setPositionY(0);
+            }
+            case L -> {
+                setPositionX(-1);
+                setPositionY(0);
+            }
+            case U -> {
+                setPositionX(0);
+                setPositionY(1);
+            }
+            case D -> {
+                setPositionX(0);
+                setPositionY(-1);
+            }
+            case UR -> {
+                setPositionX(1);
+                setPositionY(1);
+            }
+            case UL -> {
+                setPositionX(-1);
+                setPositionY(1);
+            }
+            case DR -> {
+                setPositionX(1);
+                setPositionY(-1);
+            }
+            case DL -> {
+                setPositionX(-1);
+                setPositionY(-1);
+            }
+            case NONE -> {
+                setPositionX(0);
+                setPositionY(0);
+            }
         }
+    }
+
+    public static Vector calculateVector(Position positionAfterMove, Position positionBeforeMove) {
+        return new Vector(new Position(
+                positionAfterMove.getPositionX() - positionBeforeMove.getPositionX(),
+                positionAfterMove.getPositionY() - positionBeforeMove.getPositionY()
+        ));
     }
 
     public int getValue() {
